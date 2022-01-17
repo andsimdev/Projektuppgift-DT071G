251e61b3347d963427aecc10aa0766bcc06d8cf5
andsimdev <90412106+andsimdev@users.noreply.github.com>
2022-01-17 17:37:45 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,6 @@
-/* Kod skapad av Simon Andersson för projektet i kursen DT071G, Programmering i C#.NET */
+/* Kod editerad och skapad av Simon Andersson fÃ¶r projektet i kursen DT071G, Programmering i C#.NET */
 
-// Funktionalitet för JSON
+// Funktionalitet fÃ¶r JSON
 using System.Text.Json;
 using System;
 
@@ -13,7 +13,7 @@ namespace WinFormsApp2
             InitializeComponent();
         }
 
-        // När appen startar
+        // NÃ¤r appen startar
         private void Form1_Load(object sender, EventArgs e)
         {
             // Skapa instans av klassen posthandle med referens till denna klass
@@ -29,25 +29,25 @@ namespace WinFormsApp2
             // Ange rubrik
             label3.Text = "Fem senaste framstegen";
 
-            // Dölj etiketten med instruktion för att radera post
+            // DÃ¶lj etiketten med instruktion fÃ¶r att radera post
             label4.Text = "";
 
             // Rensa Data Source (ta bort koppling till listan)
             listBox1.DataSource = null;
 
-            // Rensa listan före utskrift
+            // Rensa listan fÃ¶re utskrift
             listBox1.Items.Clear();
 
             // Skapa ny instans av klassen Posthandle
             var Posthandle = new Posthandle(this); // ta bort och flytta till klassen posthandle?
 
-            // Hämta alla poster
+            // HÃ¤mta alla poster
             var posts = Posthandle.getPosts();
 
-            // Vänd på listan för att skriva ut senaste överst
+            // VÃ¤nd pÃ¥ listan fÃ¶r att skriva ut senaste Ã¶verst
             posts.Reverse();
 
-            // Kontrollera om längden på listan understiger 5
+            // Kontrollera om lÃ¤ngden pÃ¥ listan understiger 5
             int reps;
 
             if(posts.Count < 5)
@@ -58,10 +58,10 @@ namespace WinFormsApp2
                 reps = 5;
             }
 
-            // Hämta och loopa ut de senaste 5 posterna?
+            // HÃ¤mta och loopa ut de senaste 5 posterna?
             for (int i = 0; i < reps; i++)
             {
-                // Skriv ut Datum, typ och resultat på varsina rader
+                // Skriv ut Datum, typ och resultat pÃ¥ varsina rader
                 listBox1.Items.Add(posts[i].Date);
                 listBox1.Items.Add(posts[i].Type);
                 listBox1.Items.Add(posts[i].Result);
@@ -75,22 +75,22 @@ namespace WinFormsApp2
             // Ange rubrik
             label3.Text = "Alla framsteg";
 
-            // Ange instruktioner för att radera post
-            label4.Text = "Dubbelklicka på en post för att radera";
+            // Ange instruktioner fÃ¶r att radera post
+            label4.Text = "Dubbelklicka pÃ¥ en post fÃ¶r att radera";
 
             // Skapa ny instans av klassen Posthandle
             var Posthandle = new Posthandle(this);
 
-            // Hämta alla poster
+            // HÃ¤mta alla poster
             var posts = Posthandle.getPosts();
 
-            // Vänd på arrayen för att skriva ut senaste överst
+            // VÃ¤nd pÃ¥ arrayen fÃ¶r att skriva ut senaste Ã¶verst
             posts.Reverse();
 
-            // Ange värde för varje post till dess unika Id
+            // Ange vÃ¤rde fÃ¶r varje post till dess unika Id
             listBox1.ValueMember = "Id";
 
-            // Visa postens sammansatta värde (All)
+            // Visa postens sammansatta vÃ¤rde (All)
             listBox1.DisplayMember = "All";
 
             // Bind samman listboxen med listan posts
@@ -100,10 +100,10 @@ namespace WinFormsApp2
             listBox1.ClearSelected();
         }
 
-        // Vid klick på knappen för att spara nytt framsteg
+        // Vid klick pÃ¥ knappen fÃ¶r att spara nytt framsteg
         public void button1_Click(object sender, EventArgs e)
         {
-            // Hämta inmatad data och lagra som variabler
+            // HÃ¤mta inmatad data och lagra som variabler
             var workouttype = textBox1.Text;
             var workoutdate = dateTimePicker1.Text;
             var workoutresult = textBox2.Text;
@@ -111,51 +111,51 @@ namespace WinFormsApp2
             // Skapa nytt objekt av klassen Posthandle
             var Posthandle = new Posthandle(this);
 
-            // Anropa metod för att skapa ny post, skicka med inmatad data
+            // Anropa metod fÃ¶r att skapa ny post, skicka med inmatad data
             Posthandle.addPost(workouttype, workoutdate, workoutresult);
         }
 
-        // Vid klick på knappen för att visa alla poster
+        // Vid klick pÃ¥ knappen fÃ¶r att visa alla poster
         private void button2_Click(object sender, EventArgs e)
         {
-            // Återställ meddelande
+            // Ã…terstÃ¤ll meddelande
             label8.Text = "";
 
-            // Om alla framsteg visas, gå tillbaka till "startsidan"
+            // Om alla framsteg visas, gÃ¥ tillbaka till "startsidan"
             if(label3.Text == "Alla framsteg")
             {
-                // Metoden för att skriva ut de fem senaste framstegen
+                // Metoden fÃ¶r att skriva ut de fem senaste framstegen
                 printPosts();
-                // Ändra text på knappen
+                // Ã„ndra text pÃ¥ knappen
                 button2.Text = "Visa alla";
             }
             // Annars, visa alla framsteg
             else
             {
-                // Metoden för att skriva ut alla framsteg
+                // Metoden fÃ¶r att skriva ut alla framsteg
                 printAllPosts();
-                // Ändra text på knappen
+                // Ã„ndra text pÃ¥ knappen
                 button2.Text = "Tillbaka";
             }
         }
 
-        // Vid dubbelklick på post i listan
+        // Vid dubbelklick pÃ¥ post i listan
         private void doubleClicked(object sender, EventArgs e)
         {
-            // Hämta medskickat objekt och lagra som variabel
+            // HÃ¤mta medskickat objekt och lagra som variabel
             ListBox senderLB = (ListBox)sender;
 
-            // Hämta id från klickat objekt i listan, konvertera till sträng
+            // HÃ¤mta id frÃ¥n klickat objekt i listan, konvertera till strÃ¤ng
             string itemId = Convert.ToString(senderLB.SelectedValue);
 
-            // Skapa nytt objekt från klassen Posthandle
+            // Skapa nytt objekt frÃ¥n klassen Posthandle
             var Posthandle = new Posthandle(this);
 
-            // Anropa metoden för att radera posten med aktuellt id
+            // Anropa metoden fÃ¶r att radera posten med aktuellt id
             Posthandle.deletePost(itemId);
         }
 
-        // Autogenererade eventhanterare som ej används----------
+        // Autogenererade eventhanterare som ej anvÃ¤nds----------
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e) { }
         private void label3_Click(object sender, EventArgs e) { }
         private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e) { }
@@ -164,10 +164,10 @@ namespace WinFormsApp2
         // ------------------------------------------------------
     }
 
-    // Klass för enskild post
+    // Klass fÃ¶r enskild post
     public class Post
     {
-        // Attribut för objektet (träningstyp, datum, resultat, id och en sammanslagning)
+        // Attribut fÃ¶r objektet (trÃ¤ningstyp, datum, resultat, id och en sammanslagning)
         public string? Type { get; set; }
         public string? Date { get; set; }
         public string? Result { get; set; }
@@ -175,25 +175,25 @@ namespace WinFormsApp2
         public string All { get { return Date + "  -  " + Type + "  -  " + Result; } }    
     }
 
-    // Klass för att hantera poster
+    // Klass fÃ¶r att hantera poster
     public class Posthandle
     {
-        // Fält för referens till Form 1
+        // FÃ¤lt fÃ¶r referens till Form 1
         private Form1 form;
 
         // Lagra JSON-fil som variabel
         private string file = @"posts.json";
 
-        // Skapa lista för alla poster
+        // Skapa lista fÃ¶r alla poster
         private List<Post> posts = new List<Post>();
 
-        // Konstruerare, hämta listan från JSON-filen
+        // Konstruerare, hÃ¤mta listan frÃ¥n JSON-filen
         public Posthandle(Form1 form1)
         {
-            // Tilldela objektet Form 1 till variablen form för åtkomst från denna klassen
+            // Tilldela objektet Form 1 till variablen form fÃ¶r Ã¥tkomst frÃ¥n denna klassen
             form = form1;
 
-            // Läs in JSON-filen och gör om till array
+            // LÃ¤s in JSON-filen och gÃ¶r om till array
             if (File.Exists(@"posts.json") == true)
             {
                 string jsonString = File.ReadAllText(file);
@@ -201,19 +201,19 @@ namespace WinFormsApp2
             }
         }
 
-        // Metod för att lagra ny post
+        // Metod fÃ¶r att lagra ny post
         public void addPost(string inputtype, string inputdate, string inputresult)
         {
-            // Kontrollera att inmatad data ej är tom
+            // Kontrollera att inmatad data ej Ã¤r tom
             if(String.IsNullOrEmpty(inputtype) || String.IsNullOrEmpty(inputdate) || String.IsNullOrEmpty(inputresult))
             {
                 // Skriv ut felmeddelande och stoppa metoden
                 form.label8.ForeColor = Color.Red;
-                form.label8.Text = "Alla fält måste fyllas i";
+                form.label8.Text = "Alla fÃ¤lt mÃ¥ste fyllas i";
                 return;
             }
 
-            // Skapa objekt från klassen post
+            // Skapa objekt frÃ¥n klassen post
             var post = new Post();
 
             // Lagra medskickade variabler i objektet post
@@ -221,29 +221,29 @@ namespace WinFormsApp2
             post.Date = inputdate;
             post.Result = inputresult;
 
-            // Lägg till id i form av datum, tid och millisekunder
+            // LÃ¤gg till id i form av datum, tid och millisekunder
             post.Id = DateTime.Now.ToString("MMddyyyyhhmmss.ffffff");
 
-            // Lägg till nya posten till listan
+            // LÃ¤gg till nya posten till listan
             posts.Add(post);
 
-            // Anropa metoden för att skriva ut datan till JSON-filen
+            // Anropa metoden fÃ¶r att skriva ut datan till JSON-filen
             write();
 
-            // Om alla framsteg visas, gå tillbaka till "startsidan"
+            // Om alla framsteg visas, gÃ¥ tillbaka till "startsidan"
             if (form.label3.Text == "Alla framsteg")
             {
-                // Metoden för att skriva ut alla framsteg
+                // Metoden fÃ¶r att skriva ut alla framsteg
                 form.printAllPosts();
             }
             // Annars, visa alla framsteg
             else
             {
-                // Metoden för att skriva ut de fem senaste framstegen
+                // Metoden fÃ¶r att skriva ut de fem senaste framstegen
                 form.printPosts();
             }
 
-            // Rensa inmatningsformuläret
+            // Rensa inmatningsformulÃ¤ret
             form.textBox1.Text = "";
             form.textBox2.Text = "";
 
@@ -252,35 +252,35 @@ namespace WinFormsApp2
             form.label8.Text = "Framsteget sparades!";
         }
 
-        // Metod för att hämta alla poster
+        // Metod fÃ¶r att hÃ¤mta alla poster
         public List<Post> getPosts()
         {
             // Returnerar listan med alla poster
             return posts;
         }
 
-        // Metod för att radera post
+        // Metod fÃ¶r att radera post
         public void deletePost(string id)
         {
-            // Hitta index för posten med medskickat id
-            string Id = id; // Hämta medskickat id
+            // Hitta index fÃ¶r posten med medskickat id
+            string Id = id; // HÃ¤mta medskickat id
             int index = posts.FindIndex(x => x.Id == Id);
           
             // Om ett index hittades (ej lika med -1)
             if(index != -1)
             {
-                // Radera posten med aktuellt index från listan
+                // Radera posten med aktuellt index frÃ¥n listan
                 posts.RemoveAt(index);
 
                 // Uppdatera listan till JSON-filen
                 write();
 
-                // Skriv ut listan på nytt
+                // Skriv ut listan pÃ¥ nytt
                 form.printAllPosts();
             }
         }
 
-        // Metod för att skriva ut aktuell lista till JSON-filen
+        // Metod fÃ¶r att skriva ut aktuell lista till JSON-filen
         private void write()
         {
             // Ange indenterad JSON-kod
